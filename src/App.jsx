337887426cbbd8b3f66f2b7996987f738ae69f83
Bohdan Huseinov <bohdan_huseinov@epam.com>
2018-09-12 00:51:41 +0300

    Fixed landing reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './app.less';
-import Header from '_features/header';
-import Home from '_scenes/home';
-import Landing from '_scenes/Landing';
+import Landing from '_scenes/landing';
 import './styles/common.less';
 
 export default class App extends React.Component {
