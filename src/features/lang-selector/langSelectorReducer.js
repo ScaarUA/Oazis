a60b050ea35b4b4c7709ed8bf6797de33d4b7a33
Bohdan Huseinov <bohdan_huseinov@epam.com>
2018-09-12 01:17:27 +0300

    Fixed default language

--- src/features/lang-selector/langSelectorReducer.js
+++ src/features/lang-selector/langSelectorReducer.js
@@ -1,6 +1,7 @@
 import {LANGUAGE_CHANGE} from './langSelectorConstants';
+import {langOptions} from './langSelectorConstants';
 
-const defaultState = localStorage.getItem('LANGUAGE');
+const defaultState = localStorage.getItem('LANGUAGE') || langOptions[0].value;
 
 export default function(state = defaultState, action) {
 	switch (action.type) {
